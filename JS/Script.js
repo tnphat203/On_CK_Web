1252bebe0a3d678202acbe3f4a74663ee373173b
tnphat203 <121208305+tnphat203@users.noreply.github.com>
2023-04-19 13:56:52 +0700

    Add files via upload

---
+++ JS/Script.js
@@ -0,0 +1,8 @@
+var maBN = document.querySelector('.MaBN');
+var matKhau = document.querySelector('.MatKhau');
+var ngayKham = document.querySelector('.NgayKham');
+var datLich = document.querySelector('.DatLich');
+
+datLich.addEventListener('click', function() {
+    var inMa
+});
